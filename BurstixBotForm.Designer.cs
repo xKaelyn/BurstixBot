a56809ddbbd5cdf5f6c90b7dd6d8718a78933b8e
Kaelyn <wrightricky64@gmail.com>
2023-02-19 02:07:16 +0000

    Initial commit

---
+++ BurstixBotForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace BurstixBot
+{
+    partial class BurstixBotForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BurstixBotForm));
+            this.initialiseBotButton = new System.Windows.Forms.Button();
+            this.formLogBox = new System.Windows.Forms.RichTextBox();
+            this.icaoFrom = new System.Windows.Forms.TextBox();
+            this.icaoTo = new System.Windows.Forms.TextBox();
+            this.submitICAO = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // initialiseBotButton
+            // 
+            this.initialiseBotButton.BackColor = System.Drawing.Color.HotPink;
+            this.initialiseBotButton.FlatAppearance.BorderColor = System.Drawing.Color.HotPink;
+            this.initialiseBotButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.initialiseBotButton.Location = new System.Drawing.Point(12, 12);
+            this.initialiseBotButton.Name = "initialiseBotButton";
+            this.initialiseBotButton.Size = new System.Drawing.Size(489, 23);
+            this.initialiseBotButton.TabIndex = 0;
+            this.initialiseBotButton.Text = "Initialize Bot";
+            this.initialiseBotButton.UseVisualStyleBackColor = false;
+            this.initialiseBotButton.Click += new System.EventHandler(this.initialiseBotButton_Click);
+            // 
+            // formLogBox
+            // 
+            this.formLogBox.Location = new System.Drawing.Point(12, 124);
+            this.formLogBox.Name = "formLogBox";
+            this.formLogBox.Size = new System.Drawing.Size(489, 301);
+            this.formLogBox.TabIndex = 1;
+            this.formLogBox.Text = "";
+            // 
+            // icaoFrom
+            // 
+            this.icaoFrom.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.icaoFrom.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.icaoFrom.Location = new System.Drawing.Point(13, 42);
+            this.icaoFrom.Name = "icaoFrom";
+            this.icaoFrom.Size = new System.Drawing.Size(488, 20);
+            this.icaoFrom.TabIndex = 2;
+            this.icaoFrom.Text = "FROM";
+            this.icaoFrom.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // icaoTo
+            // 
+            this.icaoTo.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.icaoTo.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.icaoTo.Location = new System.Drawing.Point(13, 69);
+            this.icaoTo.Name = "icaoTo";
+            this.icaoTo.Size = new System.Drawing.Size(488, 20);
+            this.icaoTo.TabIndex = 3;
+            this.icaoTo.Text = "TO";
+            this.icaoTo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // submitICAO
+            // 
+            this.submitICAO.BackColor = System.Drawing.Color.HotPink;
+            this.submitICAO.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.submitICAO.FlatAppearance.BorderSize = 0;
+            this.submitICAO.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.submitICAO.Location = new System.Drawing.Point(13, 95);
+            this.submitICAO.Name = "submitICAO";
+            this.submitICAO.Size = new System.Drawing.Size(488, 23);
+            this.submitICAO.TabIndex = 4;
+            this.submitICAO.Text = "Submit ICAO";
+            this.submitICAO.UseVisualStyleBackColor = false;
+            this.submitICAO.Click += new System.EventHandler(this.submitICAO_Click);
+            // 
+            // BurstixBotForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(513, 437);
+            this.Controls.Add(this.submitICAO);
+            this.Controls.Add(this.icaoTo);
+            this.Controls.Add(this.icaoFrom);
+            this.Controls.Add(this.formLogBox);
+            this.Controls.Add(this.initialiseBotButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "BurstixBotForm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "BurstixBot | Version 0.1 | Developed by iKaelyn_xo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button initialiseBotButton;
+        private System.Windows.Forms.RichTextBox formLogBox;
+        private System.Windows.Forms.TextBox icaoFrom;
+        private System.Windows.Forms.TextBox icaoTo;
+        private System.Windows.Forms.Button submitICAO;
+    }
+}
+
---
+++ BurstixBotForm.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net;
+using System.IO;
+using System.Net.Http;
+using TwitchLib.Client;
+using TwitchLib.Client.Models;
+using Newtonsoft.Json.Linq;
+using TwitchLib.Client.Events;
+using TwitchLib.Api;
+using NHttp;
+using TwitchLib.Communication.Events;
+
+namespace BurstixBot
+{
+    public partial class BurstixBotForm : Form
+    {
+
+        private HttpServer WebServer;
+        private readonly string RedirectUrl = "http://localhost";
+        private readonly string ClientId = Properties.Settings.Default.clientid;
+        private readonly string ClientSecret = Properties.Settings.Default.clientsecret;
+        private readonly List<string> Scopes = new List<string> { "chat:read", "chat:edit" };
+
+        private TwitchClient OwnerOfChannelConnection;
+        private TwitchAPI twitchAPI;
+
+        private string CachedOwnerOfChannelAccessToken = "needsaccesstoken";
+        private string TwitchChannelName;
+        private string TwitchChannelId;
+
+        string icaoFromVar;
+        string icaoToVar;
+
+        Dictionary<string, string> CommandsStaticResponses = new Dictionary<string, string>
+        {
+            { "route", "Displays the route that is currently being flown." }
+        };
+
+        public BurstixBotForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void initialiseBotButton_Click(object sender, EventArgs e)
+        {
+            initializeWebServer();
+
+            var authUrl = $"https://id.twitch.tv/oauth2/authorize?response_type=code&client_id={ClientId}&redirect_uri={RedirectUrl}&scope={String.Join("+", Scopes)}&state=frontend|NWt2SFl0OVplN0FKMUw5NFc5eE1aZz09&force_verify=true";
+            Process.Start(authUrl);
+        }
+
+        void initializeWebServer()
+        {
+            WebServer = new HttpServer();
+            WebServer.EndPoint = new IPEndPoint(IPAddress.Loopback, 80);
+
+            WebServer.RequestReceived += async (s, e) =>
+            {
+                using (var writer = new StreamWriter(e.Response.OutputStream))
+                {
+                    if (e.Request.QueryString.AllKeys.Any("code".Contains))
+                    {
+                        var code = e.Request.QueryString["code"];
+                        var ownerOfChannelAccessAndRefresh = await getAccessAndRefreshTokens(code);
+                        CachedOwnerOfChannelAccessToken = ownerOfChannelAccessAndRefresh.Item1;
+                        SetNameAndIdByOauthedUser(CachedOwnerOfChannelAccessToken).Wait();
+                        InitializeOwnerOfChannelConnection(TwitchChannelName, CachedOwnerOfChannelAccessToken);
+                        InitializeTwitchAPI(CachedOwnerOfChannelAccessToken);
+                    }
+                }
+            };
+
+            WebServer.Start();
+            Console.Write($"Web server started on: {WebServer.EndPoint}");
+        }
+
+        void InitializeTwitchAPI(string accessToken)
+        {
+            twitchAPI = new TwitchAPI();
+            twitchAPI.Settings.ClientId = ClientId;
+            twitchAPI.Settings.AccessToken = accessToken;
+        }
+
+        void InitializeOwnerOfChannelConnection(string username, string accessToken)
+        {
+            OwnerOfChannelConnection = new TwitchClient();
+            OwnerOfChannelConnection.Initialize(new ConnectionCredentials(username, accessToken), TwitchChannelName);
+
+            OwnerOfChannelConnection.OnConnected += Client_OnConnected;
+            OwnerOfChannelConnection.OnDisconnected += OwnerOfChannelConnection_OnDisconnected;
+            OwnerOfChannelConnection.OnChatCommandReceived += Bot_OnChatCommandReceived;
+
+            OwnerOfChannelConnection.Connect();
+        }
+
+        private void Client_OnConnected(object sender, OnConnectedArgs e)
+        {
+            Log($"User {e.BotUsername} connected (bot access)!");
+        }
+
+        private void OwnerOfChannelConnection_OnDisconnected(object sender, OnDisconnectedEventArgs e)
+        {
+            Log($"OnDisconnected event");
+        }
+
+        private void OwnerOfChannelConnection_OnLog(object sender, OnLogArgs e)
+        {
+            Log($"OnLog: {e.Data}");
+        }
+
+        private void Bot_OnChatCommandReceived(object sender, OnChatCommandReceivedArgs e)
+        {
+            string commandText = e.Command.CommandText.ToLower();
+
+            if (commandText.Equals("route", StringComparison.OrdinalIgnoreCase))
+            {
+                OwnerOfChannelConnection.SendMessage(TwitchChannelName, $"We're flying from {icaoFromVar} to {icaoToVar}!");
+            }
+
+            if (CommandsStaticResponses.ContainsKey(commandText))
+            {
+                OwnerOfChannelConnection.SendMessage(TwitchChannelName, CommandsStaticResponses[commandText]);
+            }
+        }
+
+        private void Log(string printMessage)
+        {
+            Invoke(new MethodInvoker(delegate ()
+            {
+                formLogBox.Text += printMessage + "\n";
+            }));
+            Console.WriteLine(printMessage);
+        }
+
+        async Task SetNameAndIdByOauthedUser(string accessToken)
+        {
+            var api = new TwitchLib.Api.TwitchAPI();
+            api.Settings.ClientId = ClientId;
+            api.Settings.AccessToken = accessToken;
+
+            var oautheduser = await api.Helix.Users.GetUsersAsync();
+            TwitchChannelId = oautheduser.Users[0].Id;
+            TwitchChannelName = oautheduser.Users[0].Login;
+        }
+
+        async Task<Tuple<string, string>> getAccessAndRefreshTokens(string code)
+        {
+            HttpClient client = new HttpClient();
+            var values = new Dictionary<string, string>
+            {
+                { "client_id", ClientId },
+                { "client_secret", ClientSecret },
+                { "code", code },
+                { "grant_type", "authorization_code" },
+                { "redirect_uri", RedirectUrl }
+            };
+
+            var content = new FormUrlEncodedContent(values);
+            var response = await client.PostAsync("https://id.twitch.tv/oauth2/token", content);
+            var responseString = await response.Content.ReadAsStringAsync();
+            var json = JObject.Parse(responseString);
+            return new Tuple<string, string>(json["access_token"].ToString(), json["refresh_token"].ToString());
+        }
+
+        private void BurstixBotForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (OwnerOfChannelConnection != null)
+            {
+                OwnerOfChannelConnection.Disconnect();
+            }
+
+            if (WebServer != null)
+            {
+                WebServer.Stop();
+                WebServer.Dispose();
+            }
+        }
+
+        private void submitICAO_Click(object sender, EventArgs e)
+        {
+            icaoFromVar = icaoFrom.Text;
+            icaoToVar = icaoTo.Text;
+            Log($"Route set as {icaoFromVar} to {icaoToVar}");
+        }
+    }
+}
